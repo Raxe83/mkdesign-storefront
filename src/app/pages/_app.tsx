c7f4be82fb6a931cae55128ee37b4d90672d3c26
Raxe83 <raydenstudio83@gmail.com>
2026-03-27 11:19:42 +0100

    remove dead code + unnecessary packages

--- src/app/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "../global.css"; // Passe den Pfad an, falls deine Datei woanders liegt
-
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
--- src/app/types/uploadcare-next.d.ts
+++
@@ -1,5 +0,0 @@
-// Type shim für @uploadcare/react-uploader/next (Subpath-Export)
-// Notwendig weil tsconfig moduleResolution "node" keine package.json exports-map auflöst.
-declare module "@uploadcare/react-uploader/next" {
-  export * from "@uploadcare/react-uploader";
-}
